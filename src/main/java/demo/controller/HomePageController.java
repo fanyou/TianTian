cee0b659d95fe3def956d9406dc32f2f6d8de437
fanyou <390662447@qq.com>
2019-01-03 20:53:25 +0800

    my project

---
+++ src/main/java/demo/controller/HomePageController.java
@@ -0,0 +1,20 @@
+package demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.ResourceMapping;
+
+/**
+ * @author fanyou
+ * @description
+ * @date 2018/12/23 23:06
+ **/
+@Controller
+@RequestMapping("/homePage")
+public class HomePageController {
+
+    @ResourceMapping("/index")
+    public String homePage() {
+        return "homePage";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/homePage.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Luke
+  Date: 2018/12/23
+  Time: 23:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Page Home</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
